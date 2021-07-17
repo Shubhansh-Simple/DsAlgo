5fa2b7c7b126eefc3b64b3d7fdc1c7104e99c661
Shubhansh Programmer <shubhansh7777@gmail.com>
2021-07-17 21:39:45 +0530

    DS Algo starts for interviews

---
+++ find_possible_pair_in_list_whose_sum.py
@@ -0,0 +1,21 @@
+'''
+Find possible pairs of two numbers in given list
+whose sum is lesser than the given number
+'''
+
+given_list = [ 1,3,7,9,10,11]
+max_sum = 7
+
+final_list =  []
+
+for each_item in given_list:
+    y = given_list.index(each_item) + 1
+
+    for _ in range(len(given_list) - y):
+
+        if each_item+given_list[y] < max_sum:
+            final_list.append( (each_item,given_list[y]))
+        y += 1
+
+print('Numbers whose summation is less than 7 - ',final_list)
+
