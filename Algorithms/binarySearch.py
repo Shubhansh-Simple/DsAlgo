25524cd5dc2a7981b5e2734f85649e73efe84336
Shubhansh <32585229+Shubhansh-Simple@users.noreply.github.com>
2021-11-17 07:08:30 +0000

    Update binarySearch.py
    [ 1,2,3,4,4,9,56,90 ] 
    Search - 8
    Get Idea from LeetCode.
    
    Here we don't want to get the answer in repeated index i.e. [3]

--- Algorithms/binarySearch.py
+++ Algorithms/binarySearch.py
@@ -3,29 +3,28 @@ Binary Search algorithm
 Find element - ele in array - arr
 Array must be sorted
 '''
-
-def binarySearch(arr,ele):
-    '''We find the element in sorted arr in fastest way'''
-
-    length = len(arr)
-
-    start = 0             # first element
-    end   = length-1      # last  element
-
+def binarySearch( self, numbers, length, target, currentIndex ):
+    start = 0
+    end   = length-1
+    
     while start <= end:
-
+        
         mid = (start+end)//2
-
-        if arr[mid] == ele:
-            return ele
-
-        elif arr[mid] < ele:
-            start = mid+1
-
+        if numbers[mid] == target:
+            if mid == currentIndex:        # [ 1,2,3,4,4,9,56,90 ]  <-- HERE 4 is repeated yeah man.
+                start = mid+1
+            else:
+                return mid                  # here mid is the index
+        
+        elif numbers[mid] < target:
+            start = mid + 1
+        
         else:
-            end = mid
+            end = mid-1
+        
+    return -1                           # because in case of 0, bool(0) <-- False
 
-    return -1
+    
     
 
 a = [ x for x in range(100000000) ]
